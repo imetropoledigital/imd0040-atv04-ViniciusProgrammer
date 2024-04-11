c6355c58079cb7bfb7104e9ae03b5071f5b20cfb
vinicius davi <viniprogrammer@hotmail.com>
2024-04-11 12:06:56 -0300

    Tarefa concluida

---
+++ src/main/java/br/ufrn/imd/Gerente.java
@@ -0,0 +1,31 @@
+package br.ufrn.imd;
+
+public class Gerente extends Funcionario{
+    public enum Nivel{
+        JUNIOR, PLENO, SENIOR
+    }
+
+    Nivel nivel;
+
+    public Gerente(int chMensal, double hh, Nivel nivel){
+        super(chMensal, hh);
+        this.nivel = nivel;
+    }
+
+    public double calcularSalario(){
+        double salario = super.calcularSalario();
+
+        switch (nivel){
+            case JUNIOR:
+                salario += 1000;
+                break;
+            case PLENO:
+                salario += 2000;
+                break;
+            case SENIOR:
+                salario += 3000;
+                break;
+        }
+        return salario;
+    }
+}
